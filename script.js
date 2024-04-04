b9286ef7253d32c0139a1757db5d0baf4acce49f
Jayashree Ugle <jayaugle5@gmail.com>
2024-04-04 17:44:38 -0400

    my first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Section 1
+
+// 1. Variables & Data types
+
+// A. Variables----------------------------------
+
+// Declare a new constant variable called myName and assign it your name?
+const myName = "Jayshree";
+
+// Declare a variable called favoriteLanguage and assign it the value of Python?
+let favoriteLanguage = "Python";
+
+// Declare a new variable called newFavoriteLanguage and assign it JavaScript;
+let newFavoriteLanguage = "JavaScript";
+
+// Assign the value of newFavoriteLanguage to favoriteLanguage referencing each other.
+favoriteLanguage == newFavoriteLanguage;
+
+// Finally log or print a message using a Literal String, make use of the variables myName and favoriteLanguage.
+console.log(myName);
+console.log(favoriteLanguage);
+
+// B. Strings -------------------------------------
+// Create a variable called firstVariable
+let firstVariable;
+
+// Assign it the value of the string "Hello World"
+console.log(firstVariable["hello World"]);
+console.log(`Hello World`)
+
+// Change the value of this variable to some number
+console.log(firstVariable[12345]);
+
+// Store the value of firstVariable in a new variable called secondVariable
+
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable? Log it!
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated. ex: Hello, my name is Jean Valjean
